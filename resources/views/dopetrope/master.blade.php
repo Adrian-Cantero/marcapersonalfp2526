e29c45d8ad40d316e2e3b52aa4cc8437f2fb443b
Adrian Cantero <1832726@alu.murciaeduca.es>
2025-11-11 09:15:34 +0100

    Vista home en Dopetrope

--- resources/views/dopetrope/master.blade.php
+++ resources/views/dopetrope/master.blade.php
@@ -27,6 +27,8 @@
 					<!-- Nav -->
 						<nav id="nav">
 							<ul>
+                                @section('menu')
+
 								<li class="current"><a href="index.html">Home</a></li>
 								<li>
 									<a href="#">Dropdown</a>
@@ -50,14 +52,14 @@
 								<li><a href="left-sidebar.html">Left Sidebar</a></li>
 								<li><a href="right-sidebar.html">Right Sidebar</a></li>
 								<li><a href="no-sidebar.html">No Sidebar</a></li>
+                                @show
 							</ul>
 						</nav>
 
 					<!-- Banner -->
 						<section id="banner">
 							<header>
-								<h2>Howdy. This is Dopetrope.</h2>
-								<p>A responsive template by HTML5 UP</p>
+								@yield('content')
 							</header>
 						</section>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,10 @@
 @extends('dopetrope.master')
 @section('menu')
     @parent
-    <p>¡Hola {{ $nombre ?? 'colega' }}!</p>
+    <li>¡Hola Colega!</li>
 @endsection
 
 @section('content')
-    <ul>
-        @if (count($users) === 1)
-            <li>Solo hay un usuario!</li>
-        @elseif (count($users) > 1)
-            <li>Hay muchos usuarios!</li>
-            @include('users.usersList', ['users' => $users])
-        @else
-            <li>No hay ningún usuario :(</li>
-        @endif
-    </ul>
+    <h2>Marca Personal F.P.</h2>
+    <p>Página principal</p>
 @endsection
