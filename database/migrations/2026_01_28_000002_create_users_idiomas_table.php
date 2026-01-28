1604b912304b89a814fb1234722138a38d55d3a5
Adrian Cantero <1832726@alu.murciaeduca.es>
2026-01-28 12:21:23 +0100

    Seguir en casa

--- database/migrations/2026_01_28_000002_create_users_idiomas_table.php
+++ database/migrations/2026_01_28_000002_create_users_idiomas_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('users_idiomas', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('idioma_id');
+            $table->foreign('idioma_id')->references('id')->on('idiomas')->onDelete('cascade');
             $table->timestamps();
         });
     }
