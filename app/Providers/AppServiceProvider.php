2a2f693b5e75ffeac49e72dff89a42337a8117c1
Adrian Cantero <1832726@alu.murciaeduca.es>
2026-02-05 11:30:38 +0100

    Ampliacion del Gate a propietarios de recursos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace App\Providers;
 
-use App\Models\Curriculo;
 use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -27,8 +27,13 @@ class AppServiceProvider extends ServiceProvider
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
 
-        Gate::define('update-curriculo', function (User $user, Curriculo $curriculo) {
-        return $user->id === $curriculo->user_id;
+        Gate::define('owner', function (User $user, $recurso, $ownerField = 'user_id') {
+            if (!$recurso || !$recurso->hasAttribute($ownerField)) {
+                return false;
+            }
+
+            $devuelvo = $user->id === $recurso->$ownerField;
+            return $devuelvo;
         });
     }
 }
